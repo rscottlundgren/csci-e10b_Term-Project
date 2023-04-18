00d6ccaea819306ecc5bf936c0f0331644b91e1f
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-04-18 12:44:38 -0400

    Change Comment Style & Contents
    - Changed comment style for quicker switch between old / new parser.
    - Added new URL for more comprehensive testing.

--- PostingParser.java
+++ PostingParser.java
@@ -7,24 +7,22 @@ class PostingParser {
 
     public static void main(String[] args) throws IOException {
 
-        /**
-         * // Create A New URL Object
-         * URL demo = new URL(
-         * "https://boards.greenhouse.io/rvohealth/jobs/4231267005");
-         * // Open A Connection To The URL
-         * URLConnection connection = demo.openConnection();
-         * //
-         * InputStreamReader inputStream = new
-         * InputStreamReader(connection.getInputStream());
-         * //
-         * BufferedReader reader = new BufferedReader(inputStream);
-         * //
-         * String i;
-         * //
-         * while ((i = reader.readLine()) != null) {
-         * System.out.println(i);
-         * }
-         */
+        // // Create A New URL Object
+        // URL demo = new URL(
+        // "https://boards.greenhouse.io/doordash/jobs/1541859?gh_jid=1541859#app");
+        // // Open A Connection To The URL
+        // URLConnection connection = demo.openConnection();
+        // //
+        // InputStreamReader inputStream = new
+        // InputStreamReader(connection.getInputStream());
+        // //
+        // BufferedReader reader = new BufferedReader(inputStream);
+        // //
+        // String i;
+        // //
+        // while ((i = reader.readLine()) != null) {
+        // System.out.println(i);
+        // }
 
         URL demo = new URL("https://boards.greenhouse.io/rvohealth/jobs/4231267005");
         URLConnection connection = demo.openConnection();
