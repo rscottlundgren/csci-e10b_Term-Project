730b599f4550b733d16f215ee0a33ec41a0fb0ac
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-04-18 11:41:49 -0400

    Update With Use Of New "GreenhouseParse" Class

--- PostingParser.java
+++ PostingParser.java
@@ -1,22 +1,36 @@
 import java.io.*;
 import java.net.*;
 
+import javax.swing.text.html.parser.ParserDelegator;
+
 class PostingParser {
 
     public static void main(String[] args) throws IOException {
 
-        URL demo = new URL(
-                "https://boards.greenhouse.io/rvohealth/jobs/4231267005");
+        /**
+         * // Create A New URL Object
+         * URL demo = new URL(
+         * "https://boards.greenhouse.io/rvohealth/jobs/4231267005");
+         * // Open A Connection To The URL
+         * URLConnection connection = demo.openConnection();
+         * //
+         * InputStreamReader inputStream = new
+         * InputStreamReader(connection.getInputStream());
+         * //
+         * BufferedReader reader = new BufferedReader(inputStream);
+         * //
+         * String i;
+         * //
+         * while ((i = reader.readLine()) != null) {
+         * System.out.println(i);
+         * }
+         */
 
+        URL demo = new URL("https://boards.greenhouse.io/rvohealth/jobs/4231267005");
         URLConnection connection = demo.openConnection();
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-        String i;
-
-        while ((i = reader.readLine()) != null) {
-            System.out.println(i);
-        }
-
+        InputStreamReader inputStream = new InputStreamReader(connection.getInputStream());
+        BufferedReader reader = new BufferedReader(inputStream);
+        ParserDelegator pd = new ParserDelegator();
+        pd.parse(reader, new GreenhouseParse(), false);
     }
 }
